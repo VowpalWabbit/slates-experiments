d727e065e06aad54091605ed2a6f4ba38fdf91b2
Jack Gerrits <jackgerrits95@gmail.com>
2019-10-18 16:06:22 -0400

    Add cb simulator, make loop.py easier to modify

---
+++ cb_simulator.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <random>
+#include <algorithm>
+
+#include "live_model.h"
+#include "constants.h"
+#include "parse_example_json.h"
+
+namespace rl = reinforcement_learning;
+
+struct person
+{
+  person(std::string id, std::string major, std::string hobby, std::string favorite_character, std::vector<float> dist)
+      : _id(id), _major(major), _hobby(hobby), _favorite_character(favorite_character), _topic_click_probability(dist)
+  {
+  }
+
+  std::string _id;
+  std::string _major;
+  std::string _hobby;
+  std::string _favorite_character;
+  std::vector<float> _topic_click_probability;
+
+  std::string get_features() const 
+  {
+    std::stringstream ss;
+    ss << R"("User":{"id":")" << _id << R"(","major":")" << _major << R"(","hobby":")" << _hobby
+       << R"(","favorite_character":")" << _favorite_character << R"("})";
+    return ss.str();
+  }
+
+  float get_outcome(size_t chosen_action, float draw) const
+  {
+    auto click_prob = _topic_click_probability[chosen_action];
+    if (draw <= click_prob)
+    {
+      return 1.0;
+    }
+    else
+    {
+      return 0.0;
+    }
+  }
+};
+
+std::string get_action_features(std::vector<std::string> const& actions)
+{
+  std::stringstream ss;
+  ss << R"("_multi":[)";
+  std::string joiner = "";
+  for (auto const& action : actions)
+  {
+    ss << joiner << R"({"a":{"topic":")" << action << R"("}})";
+    joiner = ",";
+  }
+  ss << "]";
+  return ss.str();
+}
+
+int main()
+{
+  rl::api_status status;
+  rl::utility::configuration config;
+  config.set(rl::name::APP_ID, "cb_simulator_test");
+  config.set(rl::name::OBSERVATION_SENDER_IMPLEMENTATION, rl::value::OBSERVATION_FILE_SENDER);
+  config.set(rl::name::INTERACTION_SENDER_IMPLEMENTATION, rl::value::INTERACTION_FILE_SENDER);
+  config.set(rl::name::DECISION_SENDER_IMPLEMENTATION, rl::value::INTERACTION_FILE_SENDER);
+  config.set(rl::name::INITIAL_EPSILON, "1.0");
+  config.set(rl::name::MODEL_SRC, rl::value::FILE_MODEL_DATA);
+  config.set(rl::name::MODEL_FILE_NAME, "./input.model");
+
+  auto const err_fn = [](const rl::api_status& status, void*) { std::cout << status.get_error_msg() << "\n"; };
+
+  rl::live_model rl(config, err_fn);
+  if (rl.init(&status) != rl::error_code::success)
+  {
+    std::cout << status.get_error_msg() << "\n";
+    return -1;
+  }
+
+  std::vector<std::string> const actions{"HerbGarden", "MachineLearning"};
+  std::vector<float> const tp1{0.3f, 0.2f};
+  std::vector<float> const tp2{0.1f, 0.4f};
+
+  std::vector<person> const people{
+      person("rnc", "engineering", "hiking", "spock", tp1), person("mk", "psychology", "kids", "7of9", tp2)};
+
+  std::default_random_engine rd{0};
+  std::mt19937 eng(rd());
+  std::uniform_real_distribution<float> click_distribution(0.0f, 1.0f);
+  std::uniform_int_distribution<size_t> context_distribution(0, people.size() - 1);
+
+  constexpr size_t num_rounds = 100;
+  auto const action_features = get_action_features(actions);
+  float reward = 0.f;
+  for (size_t i = 1; i <= num_rounds; i++)
+  {
+    auto const& person = people[context_distribution(eng)];
+    std::string const context = "{" +  person.get_features() +","+ action_features +"}";
+    rl::ranking_response rr;
+
+    if (rl.choose_rank(context.c_str(), rr, &status))
+    {
+      std::cerr << status.get_error_msg() << "\n";
+      return -1;
+    }
+
+    size_t action_id;
+    if (rr.get_chosen_action_id(action_id))
+    {
+      std::cout << status.get_error_msg() << "\n";
+      return -1;
+    }
+
+    auto const draw = click_distribution(eng);
+    auto const outcome = person.get_outcome(action_id, draw);
+    reward += outcome;
+    if (rl.report_outcome(rr.get_event_id(), outcome, &status))
+    {
+      std::cerr << status.get_error_msg() << "\n";
+      return -1;
+    }
+  }
+  std::cout << "Avg reward: " << (reward / (float)num_rounds) << "\n";
+}
--- loop.py
+++ loop.py
@@ -2,34 +2,37 @@ import os
 import subprocess
 import pathlib
 import shutil
+import sys
 
-class CommandFailed(Exception):
-  def __init__(self, args, result_code, stdout, stderr):
-    self.args = args
-    self.result_code = result_code
-    self.stdout = stdout
-    self.stderr = stderr
-    super().__init__(self,"\"{}\" failed with exit code:{}".format(" ".join(args), result_code))
+SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
 
-def check_result_throw(result):
-  if(result.returncode != 0):
-    raise CommandFailed(result.args, result.returncode, try_decode(result.stdout), try_decode(result.stderr))
+# Loop:
+#   1. Simulator runs and outputs data to disk. It uses the last produced model as input, or nothing if it is the first run
+#   2. Joiner takes this data, joins the observations and produces a cooked log. This file is persisted as <ITERATION>_joined.dsjson
+#   3. VW is run using that cooked log as the input. It takes the last trained model as input and produces a model to be used on the next run. It is persisted as <ITERATION>.model
+
+# Simulator MUST output an interaction.fb.data and observation.fb.data
+SIMULATOR_EXE = os.path.join(SCRIPT_DIR, "build/simulator")
+# Joiner must be able to find the files of the simulator and output a dsjson file
+JOINER_EXE = os.path.join(SCRIPT_DIR, "build/reinforcement_learning/test_tools/joiner/joiner.out")
+# The given arguments must accept the format outputted by the joiner
+VW_EXE = "vw"
+EXTRA_VW_ARGS = "--ccb_explore_adf --slate --epsilon 0.2 --power_t 0 -l 0.005 --save_resume"
+NUM_ITERATIONS = 20
 
 def run_command(command):
   result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  check_result_throw(result)
+  if result.returncode != 0:
+    print("\"{}\" failed with exit code:{}".format(" ".join(command), result.returncode))
+    sys.exit(result.returncode)
   return result
 
-SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
-SIMULATOR_EXE = os.path.join(SCRIPT_DIR, "build/simulator")
-VW_EXE = "vw"
-JOINER_EXE = os.path.join(SCRIPT_DIR, "build/reinforcement_learning/test_tools/joiner/joiner.out")
-
 def run_iteration(past_id, current_id):
+  # Remove existing input models without id in name as they may be stale.
   if os.path.exists("input.model"):
     pathlib.Path("input.model").unlink()
 
-  # If there is a past model, copy it into the name required for simulator
+  # If there is a past model, copy it into the name required for simulator.
   if past_id is not None:
     print("\tPast model found, using for simulator")
     dest = shutil.copyfile(past_id + ".model", "input.model")
@@ -49,15 +52,16 @@ def run_iteration(past_id, current_id):
   input_model = []
   if past_id is not None:
     input_model = ["-i", past_id + ".model"]
-  outut_model = ["-f", current_id + ".model"]
-  args = [VW_EXE, "--dsjson", "-d", data_file_name, "--ccb_explore_adf", "--slate", "--epsilon", "0.2", "--power_t", "0", "-l", "0.005", "--save_resume", "--id", current_id]
-  args.extend(outut_model)
+  output_model = ["-f", current_id + ".model"]
+  args = [VW_EXE, "--dsjson", "-d", data_file_name, "--id", current_id]
+  args.extend(EXTRA_VW_ARGS.split())
+  args.extend(output_model)
   args.extend(input_model)
   print("\t\t" + " ".join(args))
   result = run_command(args)
 
 last = None
-for i in range(20):
-  print("Iteration: " + str(i))
+for i in range(NUM_ITERATIONS):
+  print("Iteration: {}".format(i))
   run_iteration(last, str(i))
   last = str(i)
